88ce79d5b1ca10f23d4bc1ec25600f9a9c39409c
Aman <dmsammanuel@gmail.com>
2026-04-29 16:02:00 +0300

    Adrians code (not working)

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,5 +1,8 @@
+import cloudinary from "@/lib/cloudinary";
+
 import { Event } from "@/database";
 import connectDB from "@/lib/mongodb";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
@@ -31,11 +34,26 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ message: 'invalid json data format' }, { status: 400 })
         }
 
+        const file = theFormData.get('image') as File;
+        if (!file) return NextResponse.json({ message: 'image file is requiered' }, { status: 400 });
+
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevEvent' }, (error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+            }).end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
+
         const createdEvent = await Event.create(event);
         return NextResponse.json({ message: 'event created successfully', event: createdEvent }, { status: 201 })
 
-    } catch (e) {
+    } catch (e: any) {
         console.log(e);
-        return NextResponse.json({ message: 'Event creation failed', e: e instanceof Error ? e.message : 'Unknown' }, { status: 500 })
+        return NextResponse.json({ message: 'Event creation failed', e: e.message }, { status: 500 })
     }
 }
