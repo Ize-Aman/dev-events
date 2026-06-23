973eab410f968db9c2c1b6370790e99c08be6d16
Aman <dmsammanuel@gmail.com>
2026-06-23 17:31:55 +0300

    implemented deletion dialog

--- components/DeleteBtn.tsx
+++ components/DeleteBtn.tsx
@@ -3,20 +3,35 @@
 import { deleteEvent } from "@/lib/actions/event.actions";
 import { useState } from "react";
 
-const Page = ({eventSlug}: {eventSlug: string}) => {
-    const [isClicked, setIsClicked] = useState(false);
-    
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const {success, error} = await deleteEvent(eventSlug);
+import { FaCheck } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
 
-        if(success) console.log('event deleted')
-        else console.log('Can not delete event',error)
-    }
+const Page = ({ eventSlug }: { eventSlug: string }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleSubmit = async () => {
+    const { success, error } = await deleteEvent(eventSlug);
+
+    if (success) console.log('event deleted')
+    else console.log('Can not delete event', error)
+  }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}><button type="submit" onClick={async () => {await setIsClicked(true);console.log(isClicked)}} className="btn-delete">Delete</button></form>
+      <form onSubmit={handleSubmit}>
+        <div onClick={() => setIsClicked(true)} className="btn-delete">
+          {
+          isClicked
+          ? <div className="flex flex-col">
+          Are you sure?
+          <div className="flex flex-row gap-6">
+            <button type="submit" className="btn-delete"><FaCheck /></button> 
+            <button type="button" className="btn-delete" onClick={(e) => {e.stopPropagation(); setIsClicked(false);}}><RxCross2 /></button>
+          </div>
+          </div>
+          : <>Delete</>}
+        </div>
+      </form>
     </div>
   )
 };
